40d53e9705588ac8c000b8e8151200d3dd58073d
Brandon Clark <clark.brandon.98@gmail.com>
2022-09-28 15:09:53 -0700

    summary tables 2017-2020

--- queries.sql
+++ queries.sql
@@ -1,4 +1,4 @@
-SELECT * FROM data_2017;
+SELECT * FROM data_2021;
 
 -- Find number of columns for each table
 SELECT COUNT(*) 
@@ -14,4 +14,20 @@ UNION
 (SELECT * FROM data_2019)
 UNION
 (SELECT * FROM data_2020);
+
+SELECT * FROM homeless_data_2017_through_2020;
+
+SELECT "Year", "State", "Overall Homeless", "Unsheltered Homeless",
+"Overall Homeless Individuals", "Unsheltered Homeless Individuals", "Overall Homeless People in Families", "Sheltered Total Homeless People in Families",
+"Unsheltered Homeless People in Families", "Sheltered Total Chronically Homeless Individuals",
+"Unsheltered Chronically Homeless Individuals", "Overall Chronically Homeless People in Families", "Sheltered Total Chronically Homeless People in Families", 
+"Unsheltered Chronically Homeless People in Families", "Overall Homeless Veterans", "Sheltered Total Homeless Veterans", "Unsheltered Homeless Veterans", "Overall Homeless Unaccompanied Youth (Under 25)",
+"Sheltered Total Homeless Unaccompanied Youth (Under 25)", "Unsheltered Homeless Unaccompanied Youth (Under 25)"
+INTO homeless_data_2017_through_2020_summary
+FROM homeless_data_2017_through_2020;
+
+
+
+
+
 
